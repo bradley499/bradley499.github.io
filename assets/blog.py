c2c38dc2a7a48c31a3e6f482b14686b5bd17e6eb
Bradley Marshall <bradm499@gmail.com>
2021-08-02 22:36:06 +0100

    Added static blog listing generation

--- assets/blog.py
+++ assets/blog.py
@@ -6,6 +6,7 @@ import tzlocal
 def main():
 	sitemap_url = ["","contact","blog/"]
 	with open("../blog/updates.json", "r") as updates:
+		blogPosts = []
 		updates = json.load(updates)
 		updates = sorted(updates, key = lambda x: x[3], reverse = True)
 		titles = []
@@ -30,10 +31,12 @@ def main():
 			sitemap_url.append("blog/{0}".format(titles[x]))
 			with open("../blog/{0}.html".format(titles[x]), "w") as blog:
 				tags = ["<a title=\"View more posts tagged: {0}\" href=\"/blog/search/{0}\">#{0}</a>".format(tag) for tag in updates[x][2]]
+				tags = "										<li>" + "</li>\n										<li>".join(tags) + "</li>"
 				time = datetime.fromtimestamp(updates[x][3], tzlocal.get_localzone())
 				if bool(time.dst()):
 					time -= timedelta(hours=(int)(time.dst().seconds / 3600))
 				time = time.strftime("%B %-d, %Y - %H:%M")
+				blogPosts.append("\n								<a href=\"/blog/{0}\" title=\"Read the blog post &quot;{1}&quot;\">\n									<div>\n										<h3>{1}</h3>\n										<em>{2}</em>\n										<p style=\"font-style: italic;\">Click to read this blog post.</p>\n										<ul class=\"tags\">\n{3}\n										</ul>\n									</div>\n								</a>".format(titles[x], updates[x][0], time, tags))
 				content = [[]]
 				description = updates[x][0] + ": "
 				for element in updates[x][1]:
@@ -67,9 +70,13 @@ def main():
 								break
 				description += "... Post by Bradley Marshall"
 				html = Template("""<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n	<title>$title | Blog | Bradley Marshall</title>\n	<meta name=\"description\" content=\"$description\">\n	<meta name=\"viewport\" content=\"width=device-width, user-scalable=no\">\n	<link rel=\"stylesheet\" type=\"text/css\" href=\"/assets/style.css\">\n	<link rel=\"shortcut icon\" type=\"image/x-icon\" href=\"/favicon.ico\">\n	<script type=\"text/javascript\">const page = \"af94f605a5f03337965a0a2cb2e9c933\";</script>\n	<script type=\"text/javascript\" src=\"/assets/interact.js\"></script>\n	<script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-157001746-1\"></script>\n	<script>\n		window.dataLayer = window.dataLayer || [];\n		function gtag() { dataLayer.push(arguments); }\n		gtag('js', new Date());\n		gtag('config', 'UA-157001746-1');\n	</script>\n</head>\n<body class=\"generic\">\n	<div class=\"mainContent\">\n		<div id=\"headerNavigation\">\n			<a href=\"/\" id=\"appLogo\" title=\"Bradley Marshall\"></a>\n			<a href=\"/contact\" class=\"navLink\" id=\"contactLink\" title=\"Contact me\">Contact</a>\n			<a href=\"https://github.com/bradley499\" class=\"navLink\" id=\"github\" title=\"View my GitHub\"\n				target=\"_blank\"></a>\n		</div>\n		<div>\n			<div class=\"sidePadding\">\n				<section class=\"sidePadding\">\n					<div class=\"bigPad\">\n						<h1 class=\"bigCenter centerContent\" id=\"blogPostsTitle\">$title</h1>\n						<div>\n							<div>\n								$content\n							</div>\n							<div>\n								<div>\n									<ul class=\"tags\" id=\"blogPostTags\">\n$tags\n									</ul>\n								</div>\n								<div class=\"blogPostTime\">\n									<p>$time</p>\n							</div>\n						<p id=\"blogPostsMore\">Read more <a href=\"/blog/\" title=\"Read more blog posts\">blog posts</a>?</p>\n						</div>\n					</div>\n				</section>\n			</div>\n			<footer>\n				<div class=\"footerDecoration\">\n					<div class=\"leftBottom\">\n						<div></div>\n					</div>\n					<div class=\"rightBottom\">\n						<div></div>\n					</div>\n				</div>\n				<div class=\"footerNavigation\" class=\"privacy\">\n					<p class=\"noBottom\">This page uses <a href=\"/policies#cookies\" class=\"boldLink\"\n							title=\"Read the Cookie Policy\">Cookies</a> for analytical purposes.</p>\n					<em>&copy; Bradley Marshall <span id=\"copyrightYear\"></span></em>\n				</div>\n			</footer>\n		</div>\n	</div>\n</body>\n</html>""")
-				html = html.safe_substitute(title=updates[x][0], description=description, content=content, tags="										<li>" + "</li>\n										<li>".join(tags) + "</li>", time=time)
+				html = html.safe_substitute(title=updates[x][0], description=description, content=content, tags=tags, time=time)
 				blog.truncate(0)
 				blog.write(html)
+	with open("../blog/index.html", "w") as index:
+		blogPosts = "".join(blogPosts)
+		index.truncate(0)
+		index.write("<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n	<title>Blog | Bradley Marshall</title>\n	<meta name=\"description\" content=\"Blog updates published by Bradley Marshall\">\n	<meta name=\"viewport\" content=\"width=device-width, user-scalable=no\">\n	<link rel=\"stylesheet\" type=\"text/css\" href=\"/assets/style.css\">\n	<link rel=\"shortcut icon\" type=\"image/x-icon\" href=\"/favicon.ico\">\n	<script type=\"text/javascript\">const page = \"aeeb3f9beb9de9d8a40e72b74dc3ab8e\";</script>\n	<script type=\"text/javascript\" src=\"/assets/interact.js\"></script>\n	<script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-157001746-1\"></script>\n	<script>\n		window.dataLayer = window.dataLayer || [];\n		function gtag() { dataLayer.push(arguments); }\n		gtag('js', new Date());\n		gtag('config', 'UA-157001746-1');\n	</script>\n</head>\n<body class=\"generic\">\n	<div class=\"mainContent\">\n		<div id=\"headerNavigation\">\n			<a href=\"/\" id=\"appLogo\" title=\"Bradley Marshall\"></a>\n			<a href=\"/contact\" class=\"navLink\" id=\"contactLink\" title=\"Contact me\">Contact</a>\n			<a href=\"https://github.com/bradley499\" class=\"navLink\" id=\"github\" title=\"View my GitHub\"\n				target=\"_blank\"></a>\n		</div>\n		<div>\n			<div class=\"sidePadding\">\n				<section class=\"sidePadding\">\n					<div class=\"bigPad\">\n						<h1 class=\"bigCenter centerContent\" id=\"blogPostsTitle\">Blog posts</h1>\n						<div id=\"blogPosts\">\n							<div>" + blogPosts + "\n							</div>\n</div>\n</div>\n</section>\n</div>\n<footer>\n<div class=\"footerDecoration\">\n<div class=\"leftBottom\">\n	<div></div>\n</div>\n<div class=\"rightBottom\">\n	<div></div>\n</div>\n</div>\n<div class=\"footerNavigation\" class=\"privacy\">\n<p class=\"noBottom\">This page uses <a href=\"/policies#cookies\" class=\"boldLink\"\n		title=\"Read the Cookie Policy\">Cookies</a> for analytical purposes.</p>\n<em>&copy; Bradley Marshall <span id=\"copyrightYear\"></span></em>\n</div>\n</footer>\n</div>\n</div>\n</body>\n</html>")
 	with open("../sitemap.txt", "w") as sitemap:
 		urls = []
 		for url in sitemap_url:
